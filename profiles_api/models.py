d7a7cad7912d3ebf9f23e65edc7df686fd7e9f60
bjerkin <berkinislek@gmail.com>
2021-07-20 20:46:37 +0300

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+from rest_framework import status
 
 
 class UserProfileManager(BaseUserManager):
@@ -56,3 +58,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+    status_text = models.CharField(max_length = 255)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.status_text
